8ed78e17c247fdf612b900ec147241dbc6915abc
shuyuanfan <79356207+shuyuanfan@users.noreply.github.com>
2024-03-22 20:04:30 +0100

    Revert "documents"

--- Hippocampus/function/angletovector.m
+++
@@ -1,9 +0,0 @@
-function Gmma = angletovector(angle)
-%angle:2
-%vector:3
-theta = angle(1);
-psi = angle(2);
-Gmma =[cos(theta)*cos(psi);...
-        cos(theta)*sin(psi);...
-        -sin(theta)];
-end
--- Hippocampus/function/skew.m
+++
@@ -1,5 +0,0 @@
-function S = skew(lambda)
-S = [  0          -lambda(3)   lambda(2) ;
-    lambda(3)   0          -lambda(1) ;
-    -lambda(2)   lambda(1)   0         ];
-end
